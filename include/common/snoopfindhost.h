47d820c2cb08795162c4138a6275186634f08db8
gilgil <admin@gilgil.net>
2014-03-27 10:07:43 +0900

    Add SnoopFindHost class files into project

---
+++ include/common/snoopfindhost.h
@@ -0,0 +1,15 @@
+// ----------------------------------------------------------------------------
+//
+// Snoop Component Suite version 9.0
+//
+// http://www.gilgil.net
+//
+// Copyright (c) Gilbert Lee All rights reserved
+//
+// ----------------------------------------------------------------------------
+
+#ifndef __SNOOP_FIND_HOST_H__
+#define __SNOOP_FIND_HOST_H__
+
+
+#endif // __SNOOP_FIND_HOST_H__
