0963f51ade7a7806995cf0618e7f34f33bc3f466
gilgil <admin@gilgil.net>
2014-04-01 17:41:50 +0900

    Add windivertAddr property

--- include/process/snoopwritewindivert.cpp
+++ include/process/snoopwritewindivert.cpp
@@ -7,8 +7,11 @@ REGISTER_METACLASS(SnoopWriteWinDivert, SnoopProcess)
 // ----------------------------------------------------------------------------
 SnoopWriteWinDivert::SnoopWriteWinDivert(void* owner) : SnoopWinDivert(owner)
 {
-  autoRead = false;
-  flags    = WINDIVERT_FLAG_SNIFF;
+  autoRead             = false;
+  flags                = WINDIVERT_FLAG_SNIFF;
+  divertAddr.IfIdx     = 0;
+  divertAddr.SubIfIdx  = 0;
+  divertAddr.Direction = WINDIVERT_DIRECTION_OUTBOUND;
 }
 
 SnoopWriteWinDivert::~SnoopWriteWinDivert()
@@ -18,13 +21,53 @@ SnoopWriteWinDivert::~SnoopWriteWinDivert()
 
 void SnoopWriteWinDivert::copy(SnoopPacket* packet)
 {
+  if (divertAddr.IfIdx != 0) packet->divertAddr = divertAddr;
   SnoopWinDivert::write(packet->pktData, packet->pktHdr->caplen);
   emit copied(packet);
 }
 
 void SnoopWriteWinDivert::move(SnoopPacket* packet)
 {
+  if (divertAddr.IfIdx != 0) packet->divertAddr = divertAddr;
   SnoopWinDivert::write(packet->pktData, packet->pktHdr->caplen);
   packet->drop = true;
   emit moved(packet);
 }
+
+void SnoopWriteWinDivert::load(VXml xml)
+{
+  SnoopWinDivert::load(xml);
+
+  divertAddr.IfIdx     = xml.getInt("IfIdx",            divertAddr.IfIdx);
+  divertAddr.SubIfIdx  = xml.getInt("SubIfIdx",         divertAddr.SubIfIdx);
+  divertAddr.Direction = (UINT8)xml.getInt("Direction", (int)divertAddr.Direction);
+}
+
+void SnoopWriteWinDivert::save(VXml xml)
+{
+  SnoopWinDivert::save(xml);
+
+  xml.setInt("IfIdx",     divertAddr.IfIdx);
+  xml.setInt("SubIfIdx",  divertAddr.SubIfIdx);
+  xml.setInt("Direction", (int)divertAddr.Direction);
+}
+
+#ifdef QT_GUI_LIB
+void SnoopWriteWinDivert::optionAddWidget(QLayout* layout)
+{
+  SnoopWinDivert::optionAddWidget(layout);
+
+  VOptionable::addLineEdit(layout, "leIfIdx",     "IfIdx",     QString::number(divertAddr.IfIdx));
+  VOptionable::addLineEdit(layout, "leSubIfIdx",  "SubIfIdx",  QString::number(divertAddr.SubIfIdx));
+  VOptionable::addLineEdit(layout, "leDirection", "Direction", QString::number(divertAddr.Direction));
+}
+
+void SnoopWriteWinDivert::optionSaveDlg(QDialog* dialog)
+{
+  SnoopWinDivert::optionSaveDlg(dialog);
+
+  divertAddr.IfIdx     = dialog->findChild<QLineEdit*>("leIfIdx")->text().toUInt();
+  divertAddr.SubIfIdx  = dialog->findChild<QLineEdit*>("leSubIfIdx")->text().toUInt();
+  divertAddr.Direction = dialog->findChild<QLineEdit*>("leDirection")->text().toUInt();
+}
+#endif // QT_GUI_LIB
--- include/process/snoopwritewindivert.h
+++ include/process/snoopwritewindivert.h
@@ -24,6 +24,9 @@ public:
   SnoopWriteWinDivert(void* owner = NULL);
   virtual ~SnoopWriteWinDivert();
 
+public:
+  WINDIVERT_ADDRESS divertAddr;
+
 public slots:
   void copy(SnoopPacket* packet);
   void move(SnoopPacket* packet);
@@ -31,6 +34,16 @@ public slots:
 signals:
   void copied(SnoopPacket* packet);
   void moved(SnoopPacket* packet);
+
+public:
+  virtual void load(VXml xml);
+  virtual void save(VXml xml);
+
+#ifdef QT_GUI_LIB
+public: // for VOptionable
+  virtual void optionAddWidget(QLayout* layout);
+  virtual void optionSaveDlg(QDialog* dialog);
+#endif // QT_GUI_LIB
 };
 
 #endif // __SNOOP_WRITE_WIN_DIVERT_H__
